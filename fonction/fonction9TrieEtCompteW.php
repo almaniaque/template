d0ca60b0612adb73d63769b3637c4e04511f0617
davy Thiebaut <davy.du.60@hotmail.fr>
2026-04-30 16:27:55 +0200

    template a jour

--- fonction/fonction9TrieEtCompteW.php
+++ fonction/fonction9TrieEtCompteW.php
@@ -2,6 +2,46 @@
 
 
 
+function checkWorker($nomVille) {
+    include "tableau_datas.php"; 
+
+   
+    $Nom = [];
+    $Post = [];
+    $Ville = [];
+    $Salaire = [];
+    $detail = [];
+
+    foreach ($tableau as $ligne) {
+
+        if ($nomVille == "check") {
+            $Ville[] = $ligne[2];
+        }
+
+        else if ($ligne[2] == $nomVille) {
+            $Ville[] = $ligne[2];
+            $Nom[] = $ligne[0];
+            $Post[] = $ligne[1];
+            $Salaire[] = $ligne[5];
+        }
+    }
+
+    
+    if ($nomVille == "check") {
+        $detail = array_unique($Ville);
+        print_r($detail);
+    } else {
+        print "Nombre de personnes trouvées : " .count($Nom) .PHP_EOL;
+        print_r($nom);
+    }
+}
+
+$nomVille = readline("Entrez une ville ou 'check' : ");
+
+checkWorker($nomVille);
+
 ?>
+
+
 
 
